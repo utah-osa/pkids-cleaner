4c7e27bc91e52231bd736097f7922400622547e7
sodutton <sdutton@utah.gov>
2023-02-28 08:02:25 -0700

    Added charter_ids dataset

--- R/DataDoc.R
+++ R/DataDoc.R
@@ -6,6 +6,16 @@
 #'@name lea_ids
 #'@usage lea_ids
 #'@format ## `lea_ids`
+#'@source H:/Economists/Ed/KIDS/All LEAs/District IDs Master.csv
 "lea_ids"
 
-# put charter ids file here
+#'Charter ID Data
+#'A data frame with 121 rows and 11 columns
+#'
+#'@docType data
+#'@keywords datasets
+#'@name charter_ids
+#'@usage charter_ids
+#'@format ## `charter_ids`
+#'@source H:/Economists/Ed/KIDS/All Charter Schools/All/Charter IDs Master.csv
+"charter_ids"
